9f194ea06d3ec7b9348b63468a84fdc2b2bd1016
Keagan Young <keagan.young@standardbank.co.za>
2023-09-22 15:08:52 +0200

    List funkos, cards and images

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasmSample;
-using BlazorWasmSample.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped<JsonDataService>();
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
