ede514871c450d8675285c9d97a53a264fdfa09d
Keagan Young <keagan.young@standardbank.co.za>
2023-09-22 10:18:58 +0200

    ListPopVinyls

---
+++ release/net7.0/wwwroot/Data/FunkoPops.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorWasmSample.wwwroot.Data
+{
+    public class FunkoPops
+    {
+        public string handle { get; set; }
+        public string title { get; set; }
+        public string imageName { get; set; }
+        public List<string> series { get; set; }
+    }
+}
