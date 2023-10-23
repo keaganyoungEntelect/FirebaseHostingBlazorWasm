3325d34f61a6e84df3e58ef4ea8cc5566a267afe
Keagan Young <keagan.young@standardbank.co.za>
2023-10-23 09:32:18 +0200

    delete test

---
+++ release/net7.0/wwwroot/Data/FunkoPops.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorWasmSample.wwwroot.Data
+{
+    public class FunkoPops
+    {
+        public string? Id { get; set; }
+        public string? handle { get; set; }
+        public string? title { get; set; }
+        public string? imageName { get; set; }
+        public List<string>? series { get; set; }
+        public int? price { get; set; }
+    }
+}
